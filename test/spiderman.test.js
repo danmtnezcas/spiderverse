3d69cc657f6f8106f2f77a6ba4cfeb7001a27b21
danmtnezcas <danmtnezcas@outlook.com>
2022-05-04 11:58:56 -0500

    Modificando archivo spiderman.test.js, agregando 2 test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -11,8 +11,13 @@ describe("Unit Test for Spiderman Class", () => {
         expect(andrewGarfield.name).toBe("Spiderman Sony");
         expect(andrewGarfield.age).toBe(31);
         expect(andrewGarfield.actor).toBe("Andrew Garfield");
-        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.movies).toBe(2);
         expect(andrewGarfield.studio).toBe("Sony");
 
+    })
+
+    test('2) Use the method getInfo()', () =>{
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel");
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
     })
 })
