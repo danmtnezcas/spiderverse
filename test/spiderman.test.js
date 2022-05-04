a49d7077abdcbd0a97d607e8bb08f4a1c2caeda0
danmtnezcas <danmtnezcas@outlook.com>
2022-05-04 11:17:38 -0500

    Modificando archivo spiderman.test.js, creando el caso a probar

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resulOfSomething = 1 + 2;
-        expect(resulOfSomething).toBe(18);
+describe("Unit Test for Spiderman Class", () => {
+    test('1) Create an spiderman object', () => {
+
+        // Aquí escribimos el código que queremos usar tal cuál
+        // Quiero poder instanciar un objeto Spiderman con esta información de
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+
+        // Validamos que este código funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.studio).toBe("Sony");
+        
     })
 })
